2b35d822aa792108ffa44c9612d8ea69c892b25b
ljp-projects <lucasplumb@icloud.com>
2023-12-27 10:26:52 +1100

    lol

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const http = require('http');
-const hostname = 'localhost';
+const express = require('express');
+const app = express();
 const port = 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+})
+
+app.get('/test', (req, res) => {
+    res.send('Test Route!');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
